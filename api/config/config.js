1ac9d792198defc3ff1659097e9659229ec7922c
jonhayjr <jonhayjr@gmail.com>
2021-08-24 17:38:04 -0700

    Added git ignore file

--- api/config/config.js
+++
@@ -1,8 +0,0 @@
-const connectionString = 'mongodb+srv://jonhay:h9ZcADD3WizZTZ3@testcluster1.vkl7v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const DB = 'portfolio';
-const collection = 'projects';
-module.exports = {
-     connectionString,
-     DB,
-     collection
-}
